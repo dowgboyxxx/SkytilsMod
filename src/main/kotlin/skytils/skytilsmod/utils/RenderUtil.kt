e19edadc9027460f50d455b98ae3ba751c3322ae
Sychic <47618543+Sychic@users.noreply.github.com>
2021-09-12 20:13:50 -0400

    Cleanup item rendering

--- src/main/kotlin/skytils/skytilsmod/utils/RenderUtil.kt
+++ src/main/kotlin/skytils/skytilsmod/utils/RenderUtil.kt
@@ -252,6 +252,10 @@ object RenderUtil {
         GlStateManager.enableDepth()
         GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0)
         Minecraft.getMinecraft().renderItem.renderItemAndEffectIntoGUI(itemStack, x, y)
+        GlStateManager.disableDepth()
+        GlStateManager.disableBlend()
+        GlStateManager.disableRescaleNormal()
+        RenderHelper.disableStandardItemLighting()
     }
 
     /**
